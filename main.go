c790b065931543bd05f4d12a7b29ce8ea6bf3d8c
BojOnTheBeat <bjfatade@gmail.com>
2020-05-22 11:24:01 -0400

    create template handler

--- main.go
+++ main.go
@@ -3,7 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
+
+var templatesFolder = "templates"
+
+//templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join(templatesFolder, t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
