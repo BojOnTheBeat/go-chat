0e52386613bbe6d85fd353461694dac85f37ba90
BojOnTheBeat <bjfatade@gmail.com>
2020-05-22 12:38:06 -0400

    flesh out room.go

--- room.go
+++ room.go
@@ -5,4 +5,38 @@ type room struct {
 	// forward is a channel that holds incoming messages
 	// that should be forwarded to the other clients
 	forward chan []byte
+
+	// join is a channel for clients wishing to join the room
+	join chan *client
+
+	// leave is a channel for clients wishing to leave the room
+	leave chan *client
+
+	// client is a map that holds all the clients currently in the room
+	clients map[*client]bool
+
+	// we have two (join and leave) channels to allow us safely add and remove clients
+	// from the clients map. We don't want two goroutines to allow us modify
+	// the map at the same time
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// joining
+			r.clients[client] = true
+
+		case client := <-r.leave:
+			//leaving
+			delete(r.clients, client)
+			close(client.send)
+
+		case msg := <-r.forward:
+			// forward message to all clients
+			for client := range r.clients {
+				client.send <- msg
+			}
+		}
+	}
 }
